78bfb10bd1a0e1df13eb02136fe4b8c3aff9ffe3
Roman M <roma1109200@gmail.com>
2020-12-18 17:45:27 +0700

    corrected all files

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,30 @@
 from .pages.main_page import MainPage
+from .pages.basket_page import BasketPage
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 import time
+import pytest
 
-def test_guest_can_go_to_login_page(browser):
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+ @pytest.mark.login
+ def test_guest_can_go_to_login_page(self,browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()
+
+ @pytest.mark.basket
+ def test_guest_cant_see_product_in_basket_opened_from_main_page(self,browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    link2 = "http://selenium1py.pythonanywhere.com/ru/basket/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_basket_page()          # выполняем метод страницы — переходим на страницу корзины
+    pageB = BasketPage(browser, link2)
+    pageB.open()    
+    pageB.should_be_basket_empty()
+    pageB.should_be_text_empty()
--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,55 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+from selenium import webdriver 
 import pytest
+import time
 
-@pytest.mark.parametrize('promo_offer',["0", "1", "2", "3", "4", "5", "6", pytest.param(7, marks=pytest.mark.xfail), "8", "9"])
-def test_guest_can_go_to_login_page(browser, promo_offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                  #открываем страницу
+    page.should_be_login_link()  #должна быть ссылка на логин
+    page.go_to_login_page()      #переходим к странице авторизации
+
+    
+@pytest.mark.need_review 
+def test_guest_can_add_product_to_basket(browser):
+       link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+       page = ProductPage(browser, link)
+       page.open()
+       page.click_add_to_basket()     #нажимаем добавить в корзину
+       
+@pytest.mark.need_review 
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                    #открываем страницу
-    page.should_be_login_link()    
-    #page.click_add_to_basket()   #нажимаем добавить в корзину
-    #page.solve_quiz_and_get_code()   #решаем пример и получаем код
-    #page.coders_at_work() #сравниваем данные при добавлении в корзину
-    
+    page.should_be_login_link()    #должна быть ссылка на логин
+    page.go_to_basket_page()       #переход к корзине
+    pageB = BasketPage(browser,browser.current_url)
+    pageB.open()    
+    pageB.should_be_basket_empty() #проверка корзины на пустоту
+    pageB.should_be_text_empty()   
+
+class TestUserAddToBasketFromProductPage():   
+    @pytest.fixture(scope="function", autouse=True)  
+    def setup(self,browser):
+       link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+       page = ProductPage(browser, link)
+       page.open()
+       page.go_to_login_page()     #переходим к странице авторизации
+       page1 = LoginPage(browser,browser.current_url)
+       page1.open()
+       email=str(time.time())+"@fakemail.org"
+       password="123errere324fdr"
+       page1.register_new_user(email,password) #Регистрация пользователя
+       page1.should_be_authorized_user()       #Проверка на авторизацию
+ 
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+       link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+       page = ProductPage(browser, link)
+       page.open()
+       page.click_add_to_basket() #Доабвление товара в корзину
